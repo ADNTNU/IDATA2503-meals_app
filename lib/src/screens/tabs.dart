33e870de0445b7b48645fa3cc3bed4bf8f8ade7e
Anders Lund <a_lund_01@hotmail.com>
2024-10-11 19:16:31 +0200

    Adds button to get random meal with filters

--- lib/src/screens/tabs.dart
+++ lib/src/screens/tabs.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/src/data/dummy_data.dart';
+import 'package:meals_app/src/models/filter.dart';
 import 'package:meals_app/src/models/meal.dart';
 import 'package:meals_app/src/models/tab_screens.dart';
 import 'package:meals_app/src/screens/categories.dart';
 import 'package:meals_app/src/screens/filters.dart';
+import 'package:meals_app/src/screens/meal_details.dart';
 import 'package:meals_app/src/screens/meals.dart';
 import 'package:meals_app/src/states/favorites_state.dart';
+import 'package:meals_app/src/states/filters_state.dart';
 import 'package:meals_app/src/widgets/main_drawer.dart';
 import 'package:provider/provider.dart';
 
@@ -45,10 +49,39 @@ class _TabsScreenState extends State<TabsScreen> {
     }
   }
 
+  void _openRandomMeal(BuildContext context, Map<Filter, bool> filters) {
+    List<Meal> filteredMeals = dummyMeals
+        .where((meal) =>
+            (!filters[Filter.glutenFree]! || meal.isGlutenFree) &&
+            (!filters[Filter.lactoseFree]! || meal.isLactoseFree) &&
+            (!filters[Filter.vegetarian]! || meal.isVegetarian) &&
+            (!filters[Filter.vegan]! || meal.isVegan))
+        .toList();
+    filteredMeals.shuffle();
+
+    if (filteredMeals.isEmpty) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No meals available with the current filters.'),
+        ),
+      );
+      return;
+    }
+
+    Meal randomMeal = filteredMeals.first;
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => MealDetailsScreen(meal: randomMeal),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     List<Meal> favoriteMeals =
         Provider.of<FavoritesState>(context).favoriteMeals;
+    Map<Filter, bool> filters = Provider.of<FiltersState>(context).filters;
 
     Widget activePage = const CategoriesScreen();
     var activePageTitle = 'Categories';
@@ -61,6 +94,12 @@ class _TabsScreenState extends State<TabsScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shuffle),
+            onPressed: () => _openRandomMeal(context, filters),
+          ),
+        ],
       ),
       drawer: MainDrawer(
         onSelectScreen: _setScreen,
